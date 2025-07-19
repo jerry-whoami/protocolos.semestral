451e8604a891f0e4a53a4c6cf1e348c044e91372
JWhoami <jerry.whoami@gmail.com>
2025-07-19 07:43:44 -0500

    minor fix

--- converter.py
+++ converter.py
@@ -1,10 +1,25 @@
-import ipaddress
+from ncclient import manager
 
-def ip_to_uint32(ip):
-    return int(ipaddress.IPv4Address(ip))
+filter = """
+<filter type="subtree">
+  <ospf-oper-data xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf-oper">
+    <ospf-state><ospf-instance><ospf-area><ospf-interface>
+      <name/><adjacency-information><neighbors>
+        <neighbor>
+          <neighbor-id/>
+          <neighbor-address/>
+          <neighbor-state/>
+          <is-adjacency-full/>
+        </neighbor>
+      </neighbors></adjacency-information>
+    </ospf-interface></ospf-area></ospf-instance></ospf-state>
+  </ospf-oper-data>
+</filter>
+"""
 
-# Example:
-router_id = "3.3.3.3"
-nbr_id = ip_to_uint32(router_id)
-print(nbr_id)  # Output: 16843009
+with manager.connect(host="x.x.x.x", port=830,
+                     username="...", password="...",
+                     hostkey_verify=False) as m:
+    resp = m.get(filter)
+    print(resp.xml)
 
